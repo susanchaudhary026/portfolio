ff91e24b3b260da7f1de174e971f46f43d44b7a5
Susan Chaudhary <susanchaudharyyy@outlook.com>
2026-06-29 16:56:20 +0545

    Add: Routes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ViewController;
 
 Route::get('/', [ViewController::class, 'index']);
-Route::get('/about', [ViewController::class, 'about']);
 Route::get('/skills', [ViewController::class, 'skills']);
+Route::get('/contact', [ViewController::class, 'contact']);
 Route::get('/portfolio', [ViewController::class, 'portfolio']);
 
 
