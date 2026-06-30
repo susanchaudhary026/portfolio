01711383dd4345bae1fa06f98a658523ae379b9d
Susan Chaudhary <susanchaudharyyy@outlook.com>
2026-06-30 17:52:04 +0545

    Modify: routes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ViewController;
 
-Route::get('/', [ViewController::class, 'index']);
-Route::get('/skills', [ViewController::class, 'skills']);
-Route::get('/contact', [ViewController::class, 'contact']);
-Route::get('/portfolio', [ViewController::class, 'portfolio']);
+Route::get('/', [ViewController::class, 'index'])->name('index');
+Route::get('/skills', [ViewController::class, 'skills'])->name('admin.skills.index');
+Route::get('/contact', [ViewController::class, 'contact'])->name('admin.contact.index');
+Route::get('/projects', [ViewController::class, 'projects'])->name('admin.projects.index');
+Route::get('/cv', [ViewController::class, 'cv'])->name('admin.cv.index');
 
 
+  
